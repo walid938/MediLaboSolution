ee2210168771567e19a57cf630a1339f697dfb4c
walid938 <w.seghdau@gmail.com>
2024-05-14 22:15:21 +0200

    initializing the patient microservice with spring boot

---
+++ microservice-patient/src/test/java/mpatients/MicroservicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package mpatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
