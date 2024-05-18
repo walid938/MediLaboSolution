2abeac1bf26513dcd8f1c95afa48448c5f094657
walid938 <w.seghdau@gmail.com>
2024-05-18 15:31:06 +0200

    initializing the gateway microservice and config application.properties

---
+++ microservice-gateway/src/main/java/mgateway/MicroserviceGatewayApplication.java
@@ -0,0 +1,13 @@
+package mgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewayApplication.class, args);
+	}
+
+}
