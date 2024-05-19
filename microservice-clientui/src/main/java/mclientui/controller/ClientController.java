4515283047ca73ff3f391f968c0b803998eea1fd
walid938 <w.seghdau@gmail.com>
2024-05-19 16:03:54 +0200

    implementation of clientui microservice (controller, dto, html css)

---
+++ microservice-clientui/src/main/java/mclientui/controller/ClientController.java
@@ -0,0 +1,12 @@
+package mclientui.controller;
+
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController
+{
+
+}
